0a7082fff4ef8ea08425567ea83e7ef74c6bf399
MYXAMOR338 <silova155@gmail.com>
2024-01-07 16:12:48 +0300

    Comments

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,7 @@
 #include <QDateTime>
 #include <QTextStream>
 
+// Подключаем заголовочные файлы и библиотеки для работы с Windows API
 extern "C" {
 #include <windows.h>
 }
@@ -13,6 +14,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    // Устанавливаем соединение между событием нажатия кнопки и соответствующим методом
     connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::onPushButtonClicked);
 }
 
@@ -21,83 +23,82 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+// Обработчик события нажатия кнопки
 void MainWindow::onPushButtonClicked()
 {
+    // Получаем текущий выбранный текст из выпадающего списка и текст из текстового поля
     QString currentText = ui->comboBox_2->currentText();
     QString user_text = ui->textEdit_2->toPlainText();
 
+    // В зависимости от выбранного текста создаем событие с соответствующим типом
     if (currentText == "Warning")
     {
-        // Создаем и регистрируем событие в журнале событий
         reportEvent(user_text, user_text, EVENTLOG_WARNING_TYPE);
-        // Выводим событие в пользовательский интерфейс
         ui->textEdit->append("Событие создано: " + currentText + "  " + user_text);
     }
     else if (currentText == "Error")
     {
-        // Создаем и регистрируем событие в журнале событий
         reportEvent(user_text, user_text, EVENTLOG_ERROR_TYPE);
-        // Выводим событие в пользовательский интерфейс
         ui->textEdit->append("Событие создано: " + currentText + "  " + user_text);
     }
     else if (currentText == "Information")
     {
-        // Создаем и регистрируем событие в журнале событий
         reportEvent(user_text, user_text, EVENTLOG_INFORMATION_TYPE);
-        // Выводим событие в пользовательский интерфейс
         ui->textEdit->append("Событие создано: " + currentText + "  " + user_text);
     }
     else if (currentText == "Success Audit")
     {
-        // Создаем и регистрируем событие в журнале событий
         reportEvent(user_text, user_text, EVENTLOG_AUDIT_SUCCESS);
-        // Выводим событие в пользовательский интерфейс
         ui->textEdit->append("Событие создано: " + currentText + "  " + user_text);
     }
     else if (currentText == "Failure Audit")
     {
-        // Создаем и регистрируем событие в журнале событий
         reportEvent(user_text, user_text, EVENTLOG_AUDIT_FAILURE);
-        // Выводим событие в пользовательский интерфейс
         ui->textEdit->append("Событие создано: " + currentText + "  " + user_text);
     }
 }
 
-
-
+// Создание и регистрация события в журнале событий Windows
 void MainWindow::reportEvent(const QString& message, const QString& additionalText, WORD eventType)
 {
+    // Регистрируем источник события
     HANDLE hEventLog = RegisterEventSourceA(NULL, "MyEventSource");
 
     if (hEventLog != NULL)
     {
+        // Подготавливаем сообщение для события
         const char* messages[] = { message.toStdString().c_str(), additionalText.toStdString().c_str() };
 
+        // Регистрируем событие в журнале
         ReportEventA(hEventLog, eventType, 0, 0, NULL, 2, 0, reinterpret_cast<const CHAR**>(messages), NULL);
 
+        // Закрываем источник события
         DeregisterEventSource(hEventLog);
 
-        // Store the event information in the eventLog QStringList
+        // Сохраняем информацию о событии в QStringList
         QString eventInfo = QString("Event Type: %1, Message: %2, Additional Text: %3")
                                 .arg(eventType)
                                 .arg(message)
                                 .arg(additionalText);
         eventLog.append(eventInfo);
-
     }
     else
     {
-        QMessageBox::critical(this, "Error", "Failed to register event source.");
+        QMessageBox::critical(this, "Ошибка", "Не удалось зарегистрировать источник события.");
     }
 }
+
+// Метод для мониторинга событий в журнале событий Windows
 void MainWindow::startEventLogMonitoring()
 {
+    // Открываем журнал событий
     HANDLE hEventLog = OpenEventLog(NULL, "Application");
 
     if (hEventLog != NULL)
     {
         EVENTLOGRECORD* pevlr;
-        BYTE bBuffer[16384];
+        // Кастомное значение использованных байт, можете настроить под свою конфигурацию
+        BYTE bBuffer[8192];
         DWORD dwRead, dwNeeded;
 
         pevlr = (EVENTLOGRECORD*)&bBuffer;
@@ -105,7 +106,7 @@ void MainWindow::startEventLogMonitoring()
         // Очищаем список событий
         eventLog.clear();
 
-        // Затем получаем все записи
+        // Получаем все записи из журнала событий
         while (ReadEventLog(hEventLog,
                             EVENTLOG_BACKWARDS_READ | EVENTLOG_SEQUENTIAL_READ,
                             0,
@@ -114,17 +115,17 @@ void MainWindow::startEventLogMonitoring()
                             &dwRead,
                             &dwNeeded))
         {
-            // Получить тип события
+            // Получаем тип события
             WORD eventType = pevlr->EventType;
 
-            // Получить время события
+            // Получаем время события
             QDateTime eventTime = QDateTime::fromMSecsSinceEpoch(static_cast<qint64>(pevlr->TimeGenerated) * 1000);
             QString formattedTime = eventTime.toString("yyyy-MM-dd hh:mm:ss");
 
-            // Получить источник события
+            // Получаем источник события
             QString source = QString(reinterpret_cast<const char*>((LPBYTE)pevlr + pevlr->StringOffset));
 
-            // Добавить последнее событие в конец списка
+            // Добавляем событие в конец списка
             QString eventInfo = QString("Время события: %1, Тип события: %2, Источник: %3")
                                     .arg(formattedTime)
                                     .arg(eventType)
@@ -132,23 +133,22 @@ void MainWindow::startEventLogMonitoring()
             eventLog.append(eventInfo);
         }
 
-        // Обновить интерфейс
+        // Обновляем интерфейс
         updateEventLogUI();
 
+        // Закрываем журнал событий
         CloseEventLog(hEventLog);
     }
     else
     {
-        QMessageBox::critical(this, "Error", "Failed to open EventLog.");
+        QMessageBox::critical(this, "Ошибка", "Не удалось открыть журнал событий.");
     }
 }
 
-
-
-
+// Метод для обновления интерфейса вывода событий
 void MainWindow::updateEventLogUI()
 {
-    // Display all events in the QTextEdit with spaces between them
+    // Выводим все события в QTextEdit с пустой строкой между ними
     for (const QString &event : eventLog)
     {
         ui->textEdit->append(event);
@@ -156,16 +156,15 @@ void MainWindow::updateEventLogUI()
     }
 }
 
+// Обработчик события нажатия кнопки для мониторинга событий
 void MainWindow::on_pushButton_2_clicked()
 {
     startEventLogMonitoring();
-    // Update the QTextEdit widget with the new event information
-
+    // Обновляем виджет QTextEdit новой информацией о событиях
 }
 
-
+// Обработчик события нажатия кнопки для очистки вывода
 void MainWindow::on_pushButton_3_clicked()
 {
     ui->textEdit->clear();
 }
-
